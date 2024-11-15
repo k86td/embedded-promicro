6b7f74340454d4188e4d2f015019b1b8b0352f83
Tristan Lepine <tristanlepine14@gmail.com>
2024-11-15 11:52:08 -0500

    initial blink program

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+const DELAY: u16 = 150;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let mut led1 = pins.led_rx.into_output();
+    let mut led2 = pins.led_tx.into_output();
+
+    loop {
+        led1.set_high();
+        led2.set_low();
+        arduino_hal::delay_ms(DELAY);
+
+        led1.set_low();
+        led2.set_high();
+        arduino_hal::delay_ms(DELAY);
+    }
+}
